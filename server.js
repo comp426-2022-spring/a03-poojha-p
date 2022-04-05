eab15b6df02631797c6ae88943866d01185e2734
Poojha Palle <poojhap@ad.unc.edu>
2022-04-05 19:09:48 -0400

    additional changes

--- server.js
+++ server.js
@@ -1,103 +1,55 @@
-//Express
-const express = require('express')
+import { createRequire } from 'module';
+import { coinFlip, coinFlips, countFlips, flipACoin } from './modules/coins.mjs';
 
-//local access to express
-const app = express()
+// require Express.js
+const require = createRequire(import.meta.url);
+const express = require('express');
+const app = express();
 
-// app object has lots of useful methods
-// get(), post(), put(), delete(), REST framework!
-//app.get('/', (req, res))
-// '/' represents root of website url
-
+// start an app server
 const args = require('minimist')(process.argv.slice(2));
+args['port'];
 
-var port = args.port || 5000
+const port = args.port||process.env.PORT||5000;
 
 const server = app.listen(port, () => {
-    console.log('App is running on port %PORT%'.replace('%PORT%', port))
-})
-
-function coinFlip() {
-    return (Math.random() > 0.5 ? 'heads' : 'tails');
-}
-
-function coinFlips(flips) {
-    let arr_coins = [];
-
-    if (!flips) {
-        arr_coins.push(coinFlip());
-    } else {
-        for (var i = 0; i < flips; i++) {
-            Math.random() > 0.5 ? arr_coins.push("heads") : arr_coins.push("tails");
-        }
-    }
-
-    return arr_coins;
-}
-
-function countFlips(array) {
-    var heads = 0;
-    var tails = 0;
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] == "heads") {
-        heads++;
-      } else {
-        tails++;
-      }
-    }
-  
-    if (heads > 0 && tails == 0) {
-      return `{ heads: ${heads}}`
-    } else if (tails > 0 && heads == 0) {
-      return `{ tails: ${tails}}`
-    } else {
-      return `{ heads: ${heads}, tails: ${tails} }`
-    }
-  }
-
-function flipACoin(call) {
-    var verdict = 'win';
-    var flip = coinFlip();
-  
-    if (!(call == flip)) {
-      verdict = 'lose';
-    }
-  
-    return `{ call: ${call}, flip: ${flip}, result: ${verdict} }`;
-  }
-
-app.get('/app/', (req, res) => {
-    //route handler
-    res.status(200).end('OK')
-    //what does this line mean? why 'text/plain?'
-    res.type('text/plain')
+    console.log('App listening on port %PORT%'.replace('%PORT%',port));
 });
 
-app.get('/app/flip', (req, res) => {
-    res.status(200)
-    res.type('text/plain')
-    res.json({'flip': coinFlip()})
+// endpoint /app/flip/
+app.get('/app/flip/', (req, res) => {
+    const flip = {"flip": coinFlip()};
+    res.status(200).json(flip);
 });
 
+// endpoint /app/flips/:number
 app.get('/app/flips/:number', (req, res) => {
-    res.status(200)
-    var flip = coinFlips(req.params.number)
-    res.json({'raw': flip, 'summary': countFlips(flip)})
+    let flips = coinFlips(req.params.number);
+    let count = countFlips(flips);
+    res.status(200).json({"raw": flips, "summary": count});
 });
 
+// endpoint /app/flip/call/heads
 app.get('/app/flip/call/heads', (req, res) => {
-    res.status(200)
-    res.json(flipACoin('heads'))    
+    const game = flipACoin('heads');
+    res.status(200).json(game);
 });
 
+// endpoint /app/flip/call/tails
 app.get('/app/flip/call/tails', (req, res) => {
-    res.status(200)
-    res.type('text/plain')
-    res.json(flipACoin('tails'))
-})
+    const game = flipACoin('tails');
+    res.status(200).json(game);
+});
 
-app.use(function(req, res) {
-    res.status(404).send('404 ERROR')
-    res.type('text/plain')
-})
+// check endpoint
+app.get('/app/', (req, res) => {
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, { 'Content-Type' : 'text/plain' });
+    res.end(res.statusCode+ ' ' +res.statusMessage);
+});
 
+// default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND');
+});
