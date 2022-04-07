47dcf92ed54da156bb77d8a19f3992256ff77b58
Poojha Palle <poojhap@ad.unc.edu>
2022-04-07 01:34:56 -0400

    not module type anymore

--- server.js
+++ server.js
@@ -1,18 +1,67 @@
-import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
+//import {coinFlip, coinFlips, countFlips, flipACoin} from "./modules/coin.mjs"
+//import { createRequire } from 'module';
 
 const express = require('express')
 const app = express()
 
 const args = require('minimist')(process.argv.slice(2))
 
-const port = args['port'] || 5000
+args['port']
+
+const port = args['port'] || process.env.PORT || 5000
 
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
+
+function coinFlip() {
+    return (Math.random() > 0.5 ? 'heads' : 'tails');
+}
+
+function coinFlips(flips) {
+    let arr_coins = [];
+  
+    if (!flips) {
+      arr_coins.push(coinFlip());
+    } else {
+      for (var i = 0; i < flips; i++) {
+        Math.random() > 0.5 ? arr_coins.push("heads") : arr_coins.push("tails");
+      }
+    }
+  
+    return arr_coins;
+}
+
+function countFlips(array) {
+    var heads = 0;
+    var tails = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == "heads") {
+        heads++;
+      } else {
+        tails++;
+      }
+    }
+  
+    if (heads > 0 && tails == 0) {
+      return `{ heads: ${heads}}`
+    } else if (tails > 0 && heads == 0) {
+      return `{ tails: ${tails}}`
+    } else {
+      return `{ heads: ${heads}, "tails": ${tails} }`
+    }
+}
+
+function flipACoin(call) {
+    var verdict = 'win';
+    var flip = coinFlip();
+  
+    if (!(call == flip)) {
+      verdict = 'lose';
+    }
+  
+    return `{ call: ${call}, flip: ${flip}, result: ${verdict} }`;
+}
 
 app.get('/app/', (req, res) => {
     res.statusCode = 200;
