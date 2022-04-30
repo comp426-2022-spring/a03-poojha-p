d04533b6e628a7c718263b2282f0d95a31fda7db
Poojha Palle <poojhap@ad.unc.edu>
2022-04-30 00:38:32 -0400

    coin flips

--- server.js
+++ server.js
@@ -17,3 +17,17 @@ const server = app.listen(port, () => {
 function coinFlip() {
   return (Math.random() > 0.5 ? 'heads' : 'tails');
 }
+
+function coinFlips(flips) {
+  let arr_coins = [];
+
+  if (!flips) {
+    arr_coins.push(coinFlip());
+  } else {
+    for (var i = 0; i < flips; i++) {
+      Math.random() > 0.5 ? arr_coins.push("heads") : arr_coins.push("tails");
+    }
+  }
+
+  return arr_coins;
+}
